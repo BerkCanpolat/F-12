c702a59af56b86bb539117c26419cba1cf7c60e5
BerkCanpolat <canpolatberk19@gmail.com>
2023-04-07 22:19:45 +0300

    add files

--- f12/lib/main.dart
+++ f12/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:f12/Screen/Anasayfa.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Anasayfa(),
     );
   }
 }
