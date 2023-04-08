0ef39e63bcab5e59c5e8935896f2639f5c4d2099
Fatih ÖZCAN <fatihozcan1461@gmail.com>
2023-04-08 16:02:56 +0300

    create login

--- f12/lib/main.dart
+++ f12/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:f12/Login/Login.dart';
 import 'package:f12/Screen/Anasayfa.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -8,14 +10,20 @@ Future<void> main() async{
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Anasayfa(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    navigatorKey: navigatorKey,
+    debugShowCheckedModeBanner: false,
+    home:Giris() ,
+
+
+  );
+
+
 }
+
