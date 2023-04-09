e91aa7d2ba2e45ecd612cff472542158adb8bca9
Ceylin <caltepeceylin@gmail.com>
2023-04-09 23:10:21 +0300

    Create setting page

--- f12/lib/Screen/Ayarlar.dart
+++ f12/lib/Screen/Ayarlar.dart
@@ -13,7 +13,180 @@ class _AyarlarState extends State<Ayarlar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            color: Colors.grey[300],
+            margin: EdgeInsets.all(10),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              side: BorderSide(
+                color: Colors.deepPurple,
+                width: 2,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text(
+                  "Profil resminizi değiştirin",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Icon(Icons.account_circle),
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.grey[300],
+            margin: EdgeInsets.all(10),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              side: BorderSide(
+                color: Colors.deepPurple,
+                width: 2,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text(
+                  "İsminizi değiştirin",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Icon(Icons.title),
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.grey[300],
+            margin: EdgeInsets.all(10),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              side: BorderSide(
+                color: Colors.deepPurple,
+                width: 2,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text(
+                  "Şifrenizi değiştirin",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Icon(Icons.key),
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.grey[300],
+            margin: EdgeInsets.all(10),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              side: BorderSide(
+                color: Colors.deepPurple,
+                width: 2,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text(
+                  "Kullanıcı adınızı değiştirin",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Icon(Icons.supervisor_account),
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.grey[300],
+            margin: EdgeInsets.all(10),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              side: BorderSide(
+                color: Colors.deepPurple,
+                width: 2,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text(
+                  "Favorilerinizi düzenleyin",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.grey[300],
+            margin: EdgeInsets.all(10),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              side: BorderSide(
+                color: Colors.deepPurple,
+                width: 2,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text(
+                  "Bildirimlerinizi açın/kapayın",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Icon(Icons.notifications),
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.grey[300],
+            margin: EdgeInsets.all(10),
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              side: BorderSide(
+                color: Colors.deepPurple,
+                width: 2,
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text(
+                  "Temanızı değiştirin",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Icon(Icons.add_home),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
